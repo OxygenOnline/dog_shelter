0d770b073e86a78eec8ec533d6057cfb374de869
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2023-11-10 15:23:16 +0100

    minor fixes

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -5,11 +5,11 @@ const Navbar: React.FC = () => {
         <nav>
             <div className='flex justify-end align-end text-lg'>
                 <ul className='flex'>
-                    <li className='p-4 hover:text-slate-700'>
-                        <Link href='/login'>Login</Link>
+                    <li className='p-4'>
+                        <Link href='/login' className='hover:text-slate-700'>Login</Link>
                     </li>
-                    <li className='p-4 hover:text-slate-700'>
-                        <Link href='/register'>Register</Link>
+                    <li className='p-4'>
+                        <Link href='/register' className='hover:text-slate-700'>Register</Link>
                     </li>
                 </ul>
             </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,9 +4,10 @@ import ShelterButton from './components/ShelterButton';
 import DogButton from './components/DogButton';
 
 export default function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+
       <Navbar />
 
       <div className="relative flex place-items-center p-24">
@@ -17,16 +18,15 @@ export default function Home() {
           width={256}
           height={256}
           priority
-          // image source: https://www.flaticon.com
+        // image source: https://www.flaticon.com
         />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-center">
-
-      <ShelterButton />
-      <DogButton />
-
+        <ShelterButton />
+        <DogButton />
       </div>
+      
     </main>
   )
 }
