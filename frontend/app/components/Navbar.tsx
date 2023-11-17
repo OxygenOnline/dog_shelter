15106684f0debfd2af24df04ded9b6dabb5878be
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2023-11-17 14:38:35 +0100

    home section added to nav

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import { CgHome } from "react-icons/cg";
 
 const Navbar: React.FC = () => {
     return (
-        <nav>
-            <div className='flex justify-end align-end text-lg'>
+        <nav className='flex flex-row justify-between'>
+            <div className='flex text-lg'>
+                <Link href='/' className='flex justify-center items-center mx-4 hover:text-slate-700'>
+                    <CgHome size={28} className="inline-block align-middle" />
+                </Link>
+            </div>
+            <div className='flex text-lg'>
                 <ul className='flex'>
                     <li className='p-4'>
                         <Link href='/login' className='hover:text-slate-700'>Login</Link>
