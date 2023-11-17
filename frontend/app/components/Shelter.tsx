ea4081cbc1134c19466db7737fbf1c171d44f4a5
Farkas Fruzsina Diána <fruzsinadiana@gmail.com>
2023-11-17 11:46:37 +0100

    shelter edit implemented

--- frontend/app/components/Shelter.tsx
+++ frontend/app/components/Shelter.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { FormEventHandler, useState } from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { FiEdit } from "react-icons/fi";
 import { Shelter } from "../types/shelters"
 import Modal from "./Modal";
-import { deleteShelter } from "../api"
+import { deleteShelter, editShelter } from "../api"
 
 
 interface ShelterProps {
@@ -15,13 +15,30 @@ interface ShelterProps {
 
 export const ShelterRow: React.FC<ShelterProps> = ({ shelter }) => {
     const router = useRouter();
+    const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
     const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+    const [shelterId] = useState(shelter.shelterId);
+    const [shelterName, setShelterName] = useState(shelter.shelterName);
+    const [location, setLocation] = useState(shelter.location);
+    const [email, setEmail] = useState(shelter.email);
 
     const handleDeleteShelter = async (id: number) => {
         await deleteShelter(id);
         setOpenModalDeleted(false);
         router.refresh();
     }
+
+    const handleSubmitEditShelter: FormEventHandler<HTMLFormElement> = async () => {
+        const editedShelter = {
+            shelterId,
+            shelterName,
+            location,
+            email
+        }
+        await editShelter(editedShelter);
+        setOpenModalEdit(false);
+        router.refresh();
+    };
 
     return (
         <tr key={shelter.shelterId}
@@ -32,9 +49,70 @@ export const ShelterRow: React.FC<ShelterProps> = ({ shelter }) => {
             <td>{shelter.email}</td>
             <td className="px-2">
                 <FiEdit
+                    onClick={() => setOpenModalEdit(true)}
                     size={24}
                     cursor='pointer'
                     className="hover:text-slate-700" />
+                <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+                    <form onSubmit={handleSubmitEditShelter}>
+                        <h3 className='font-bold text-lg'>Edit Shelter</h3>
+                        <div className='modal-action flex flex-col'>
+                            <div className='mb-4'>
+                                <label htmlFor='shelterId' className='block text-sm font-medium text-gray-400'>
+                                    Shelter ID
+                                </label>
+                                <input
+                                    id='shelterId'
+                                    value={shelter.shelterId}
+                                    readOnly
+                                    className='input input-bordered w-full bg-gray-700 cursor-not-allowed'
+                                />
+                            </div>
+                            <div className='mb-4'>
+                                <label htmlFor='shelterName' className='block text-sm font-medium text-gray-400'>
+                                    Shelter Name
+                                </label>
+                                <input
+                                    id='shelterName'
+                                    value={shelterName}
+                                    onChange={(e) => setShelterName(e.target.value)}
+                                    type='text'
+                                    placeholder='Enter Shelter Name'
+                                    className='input input-bordered w-full'
+                                />
+                            </div>
+                            <div className='mb-4'>
+                                <label htmlFor='location' className='block text-sm font-medium text-gray-400'>
+                                    Location
+                                </label>
+                                <input
+                                    id='location'
+                                    value={location}
+                                    onChange={(e) => setLocation(e.target.value)}
+                                    type='text'
+                                    placeholder='Enter Location'
+                                    className='input input-bordered w-full'
+                                />
+                            </div>
+                            <div className='mb-4'>
+                                <label htmlFor='email' className='block text-sm font-medium text-gray-400'>
+                                    Email
+                                </label>
+                                <input
+                                    id='email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    type='email'
+                                    placeholder='Enter Email'
+                                    className='input input-bordered w-full'
+                                />
+                            </div>
+                            <button type='submit' className='btn'>
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </Modal>
             </td>
             <td className="px-2">
                 <RiDeleteBin6Line
